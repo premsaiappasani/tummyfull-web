a32e68d6f9f29513e4a9b39b543eb792b412116a
teuppulu2401 <tejo.uppuluri@nielsen.com>
2025-04-20 19:21:22 +0530

    all views

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from db.connection import get_db
+from controllers import register_blueprints
+import os
+import datetime
 
 def create_app():
-    app = Flask(__name__)
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    views_path = os.path.join(project_root, 'views')
+    app = Flask(__name__, template_folder=views_path)
+    app.debug = True
     app.config.from_object('app.config.Config')
+    app.permanent_session_lifetime = datetime.timedelta(hours=1)
 
     app.db = get_db()
 
-    from controllers import register_blueprints
+    
     register_blueprints(app)
 
     return app
